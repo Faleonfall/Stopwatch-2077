3b0be3228be65cdb2852faff39ac95433d127cff
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2025-11-08 09:52:49 +0100

    Add robots.txt and fix iOS scroll issue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-cyber-background">
+  <div class="flex items-center justify-center bg-cyber-background overflow-hidden h-[100svh]">
     <Stopwatch/>
   </div>
 </template>
